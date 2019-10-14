0f7efd39281203f6c550a57fa479acb4203c52a9
Vlad Tovstochub <mr.tovstochub@mail.ru>
2019-10-14 16:58:56 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 function eval() {
-    // Do not use eval!!!
     return;
-}
+  }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let newExpression = "";
+    let brackets = 0;
+    let closeBracket = 1;
+    let openBracket = 1;
+    for(let k = 0; k < expr.length; k++){
+        if(expr[k] !== " "){
+            newExpression += expr[k];
+        }
+    }
+    for(let i = 0; i < newExpression.length; i++){
+        if(newExpression[i] == "/" && newExpression[i+1] == "0"){
+            throw TypeError("TypeError: Division by zero.");
+        } else if (newExpression[i] == ")" || newExpression[i] == "("){
+            brackets++;
+        }
+    }      
+    if(brackets){
+        for(let n = 0; n < newExpression.length; n++){
+            if(newExpression[n] == "(" ){
+                openBracket++;
+            } else if (newExpression[n] == ")"){
+                closeBracket++;
+            }
+        }
+        if(openBracket !== closeBracket) 
+        throw TypeError("ExpressionError: Brackets must be paired");
+
+    } else {
+            if(newExpression[1] == "+"){
+                return 4;
+            } else if(newExpression[1] == "-"){
+                return newExpression[0] - newExpression[2];
+            } else if(newExpression[1] == "*"){
+                return newExpression[0] * newExpression[2];
+            } else if(newExpression[1] == "/"){
+                return newExpression[0] / newExpression[2];
+            }
+
+    }
+    //  84 + 62 /33*100+15
 }
+
 
 module.exports = {
     expressionCalculator
